31d3d68d3b31a0f0e2978ce603f0d4478b1ef9f5
Ben Chong <benchong1999@yahoo.com.au>
2022-06-27 16:03:52 +1000

    Removed default Gatsby content and styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,41 @@
 module.exports = {
   siteMetadata: {
     title: `benchong.dev`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://benchong.dev`,
+    author: `Ben Chong`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Poppins:400,700`
+        ],
+        display: 'swap'
+      }
+    }
+  ]
 };
