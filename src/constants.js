5e998fb85ad72fa23107f08b2411c2f57e0c0289
Ben Chong <benchong1999@yahoo.com.au>
2022-06-29 14:59:39 +1000

    Add colour and font weight constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+    primary: 'hsl(59deg, 100%, 100%)',
+    secondary: 'hsl(237deg, 31%, 86%)',
+    tertiary: 'hsl(237deg, 31%, 86%)',
+    highlight: 'hsl(246deg, 72%, 60%)',
+    text: 'hsl(232deg, 9%, 18%)'
+}
+
+export const WEIGHTS = {
+    normal: 400,
+    bold: 700
+}
